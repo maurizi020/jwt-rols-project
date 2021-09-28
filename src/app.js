5e72c07b5bd9752f9db18938814fa66b1b729577
Michele Sanseviero <msanseviero1195@gmail.com>
2021-09-28 19:42:07 -0400

    set pkg value in express

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
+import pkg from '../package.json';
 
 const app = express();
 app.use(morgan('dev'));
+app.set('pkg', pkg);
 
 
 app.get("/", (req, res) => {
