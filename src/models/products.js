9f4a1bad1dab82b61a6f1e282f84c9e3ef5da3b9
Michele Sanseviero <msanseviero1195@gmail.com>
2021-09-29 16:00:00 -0400

    add validates to models

--- src/models/products.js
+++ src/models/products.js
@@ -1,10 +1,28 @@
 import { Schema, model } from 'mongoose';
 
+const isEmpty = (v) => v !== '';
+
 const productSchema = new Schema({
-  name: String,
-  category: String,
-  price: Number,
-  imgURL: String,
+  name: {
+    type: String,
+    required: true,
+    validate: isEmpty,
+  },
+  category: {
+    type: String,
+    required: true,
+    validate: isEmpty,
+  },
+  price: {
+    type: Number,
+    required: true,
+    validate: (v) => v !== 0,
+  },
+  imgURL: {
+    type: String,
+    required: true,
+    validate: isEmpty,
+  },
 }, {
   timestamps: true,
   versionKey: false,
