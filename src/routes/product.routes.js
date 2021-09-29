66dd15c1ef1ae19858d36811cf21976bb78e6f7f
Michele Sanseviero <msanseviero1195@gmail.com>
2021-09-29 17:53:30 -0400

    add create preduct endpoint

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,5 +4,6 @@ import * as productsController from '../controllers/products.controller';
 const router = Router();
 
 router.get('/', productsController.getProducts);
+router.post('/created', productsController.createProduct);
 
 export default router;
