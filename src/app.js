4febfaead0a2fadcb403af5ace6f054d41bbb31a
Michele Sanseviero <msanseviero1195@gmail.com>
2021-09-28 20:26:18 -0400

    init eslint

--- src/app.js
+++ src/app.js
@@ -7,16 +7,15 @@ const app = express();
 app.use(morgan('dev'));
 app.set('pkg', pkg);
 
-
-app.get("/", (req, res) => {
-    res.json({
-      message: "Welcome to my Products API",
-      name: app.get("pkg").name,
-      version: app.get("pkg").version,
-      description: app.get("pkg").description,
-      author: app.get("pkg").author,
-    });
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to my Products API',
+    name: app.get('pkg').name,
+    version: app.get('pkg').version,
+    description: app.get('pkg').description,
+    author: app.get('pkg').author,
   });
+});
 app.use('/products', products);
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import app from './app';
 const port = process.env.SERVER_PORT || 4000;
 
 app.listen(port, () => {
-    console.log('Escuchando en el puerto', port);
+  // eslint-disable-next-line no-console
+  console.log('Escuchando en el puerto', port);
 });
