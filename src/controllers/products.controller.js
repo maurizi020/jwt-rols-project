6a6840e1257aab4ea07f7ae1921e22a34f4257c4
Michele Sanseviero <msanseviero1195@gmail.com>
2021-09-29 16:00:21 -0400

    add createProdutc

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,5 +1,23 @@
-const createProduct = (req, res) => {
+import DEBUG from 'debug';
+import { StatusCode } from 'http-status-codes';
+import Products from '../models/products';
 
+const debug = DEBUG('Controllers:Products');
+
+const createProduct = async (req, res) => {
+  const {
+    name, category, price, imgURL,
+  } = req.body;
+  const newProduct = new Products({
+    name, category, price, imgURL,
+  });
+  try {
+    await newProduct.save();
+    res.status(StatusCode.CREATED).send({ msg: 'the product has been created.' });
+  } catch (error) {
+    debug(error);
+    res.status(StatusCode.INTERNAL_SERVER_ERROR).json({ msg: 'internal server error' });
+  }
 };
 
 const getProducts = (req, res) => {
