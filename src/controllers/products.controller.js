85419ac2cea92b5cce444b8d74ada6add8bb9731
Michele Sanseviero <msanseviero1195@gmail.com>
2021-09-28 20:01:23 -0400

    init products controller

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,27 @@
+const createProduct = (req, res) => {
+
+}
+
+const getProducts = (req, res) => {
+
+}
+
+const getProductById = (req, res) => {
+
+}
+
+const updateProductById = (req, res) => {
+
+}
+
+const deleteProductById = (req, res) => {
+
+}
+
+export {
+    createProduct,
+    getProducts,
+    getProductById,
+    updateProductById,
+    deleteProductById
+};
