9e84506b0a28e54f30e57d2947a0e0ab90b92a7e
Michele Sanseviero <msanseviero1195@gmail.com>
2021-09-30 20:51:56 -0400

    add endpoints

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import * as authControllers from '../controllers/auth.controller';
 
 const router = Router();
+
+router.post('/SingUp', authControllers.SingUp);
+router.post('/SingIn', authControllers.SingIn);
 
 export default router;
